773693327c4c3baf34e2c432d80ba24de620fe1c
Maryam <maryamgadiali@outlook.com>
2025-12-20 17:30:28 +0000

    Created new sprocs to get attendance summaries

---
+++ College Information and Reporting System/SQL Stored Procedures/usp_GetAttendanceRateForCourse.sql
@@ -0,0 +1,28 @@
+﻿USE [CIRS]
+GO
+/****** Object:  StoredProcedure [dbo].[usp_GetAttendanceRateForCourse]    Script Date: 20/12/2025 17:16:05 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Maryam.G
+-- Create date: 20/12/2025
+-- Description:	Gets the attendance rate for a specific course
+-- Returns: Present, Absent, Late, Authorised absence amount for a specific course
+-- =============================================
+CREATE PROCEDURE [dbo].[usp_GetAttendanceRateForCourse]
+	@courseId INT
+AS
+BEGIN
+	SELECT 
+	c.courseId, c.courseName,
+	count(*) as totalSessions,
+	SUM(case when a.attendanceStatus=0 Then 1 else 0 end) as [Present],
+	SUM(case when a.attendanceStatus=1 Then 1 else 0 end) as [Absent],
+	SUM(case when a.attendanceStatus=2 Then 1 else 0 end) as [Late],
+	SUM(case when a.attendanceStatus=3 Then 1 else 0 end) as [AuthorisedAbsence]
+	from attendances a left join courses c on a.courseId=c.courseId
+	where a.courseId=@courseId
+	group by c.courseId, c.courseName;
+END
---
+++ College Information and Reporting System/SQL Stored Procedures/usp_GetStudentAttendanceSummary.sql
@@ -0,0 +1,30 @@
+﻿USE [CIRS]
+GO
+/****** Object:  StoredProcedure [dbo].[usp_GetStudentAttendanceSummary]    Script Date: 20/12/2025 17:28:33 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Maryam.G
+-- Create date: 18/12/2025
+-- Description:	Gets the attendance rows for a specific student
+-- Returns: Present, Absent, Late, Authorised absence amount for student
+-- =============================================
+CREATE PROCEDURE [dbo].[usp_GetStudentAttendanceSummary]
+	@studentId INT
+AS
+BEGIN
+	SELECT 
+	s.studentId, s.studentfirstname, s.studentLastName,
+	count(a.attendanceId) as TotalSessions,
+	SUM(case when a.attendanceStatus=0 Then 1 else 0 end) as [Present],
+	SUM(case when a.attendanceStatus=1 Then 1 else 0 end) as [Absent],
+	SUM(case when a.attendanceStatus=2 Then 1 else 0 end) as [Late],
+	SUM(case when a.attendanceStatus=3 Then 1 else 0 end) as [AuthorisedAbsence]
+
+	from Students s 
+	left join Attendances a ON s.studentId=a.studentId
+	where s.studentId=@studentId
+	GROUP BY s.StudentId, studentFirstName, studentLastName;
+END
