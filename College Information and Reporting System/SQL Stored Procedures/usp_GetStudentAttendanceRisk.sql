828afce40926195caf7ab537bf1395d23fe75dd2
Maryam <maryamgadiali@outlook.com>
2025-12-21 18:44:08 +0000

    Added new sprocs and a view table

---
+++ College Information and Reporting System/SQL Stored Procedures/usp_GetStudentAttendanceRisk.sql
@@ -0,0 +1,47 @@
+﻿USE [CIRS]
+GO
+/****** Object:  StoredProcedure [dbo].[usp_GetStudentAttendanceRisk]    Script Date: 20/12/2025 21:49:12 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Maryam.G
+-- Create date: 20/12/2025
+-- Description:	Outputs each student and their attendance risk level:
+--				>85 - Acceptable
+--				70 - 85 - Monitor
+--				<70 (or user can enter optional risk amount) - At risk
+
+-- =============================================
+CREATE PROCEDURE [dbo].[usp_GetStudentAttendanceRisk]
+	@startDate date = '1900-01-01',
+	@endDate date = '9999-12-31',
+	@riskLevel decimal(5,2) = 70
+AS
+BEGIN
+	--Gathers all attendance summaries for each student within a certain date interval
+	with attendanceCTE as(
+		 select
+		 s.studentid, s.studentFirstName, s.studentLastName,
+		 count(a.AttendanceId) as TotalSessions,
+         sum(case when a.AttendanceStatus = 0 then 1 else 0 end) as PresentSessions
+		 from attendances a left join students s on a.studentid=s.studentid
+	     where a.attendanceTime between @startdate and @enddate
+		 group by s.studentid, s.studentfirstname,s.studentlastname
+	)
+
+	--Selects Risk level based off the attendance summaries and outputs it
+	SELECT 
+	studentid, studentfirstname,studentlastname,
+	cast((PresentSessions*100.00 / nullif(TotalSessions,0)) as decimal(5,2)) as attendanceAvg,
+	case 
+		when (cast((PresentSessions*100.00 / nullif(TotalSessions,0)) as decimal(5,2))) < @riskLevel then 'At risk'
+		when (cast((PresentSessions*100.00 / nullif(TotalSessions,0)) as decimal(5,2))) between 70 and 85 then 'Monitor'
+		else 'Acceptable'
+	end as 'RiskStatus'
+	from attendanceCTE
+	order by attendanceAvg asc;
+
+
+END
---
+++ College Information and Reporting System/SQL Stored Procedures/vw_courseOverview.sql
@@ -0,0 +1,39 @@
+﻿
+create view vw_courseOverview
+as
+SELECT
+    fq.courseName,
+    fq.courseId,
+    fq.departmentName,
+    fq.departmentCode,
+    fq.departmentHead,
+    fq.totalStudents,
+    fq.budgetPerStudent,
+    sq.avgAttendance
+FROM (
+
+	--First query
+	select 
+	c.courseName,c.courseId,d.departmentName,d.departmentCode,d.departmentHead,
+	COUNT(cs.studentsstudentId) as totalStudents,
+	cast(c.budget/CAST(COUNT(cs.studentsstudentId)as decimal(10,2)) as decimal(10,2)) as budgetPerStudent
+	from courses c inner join departments d on c.departmentId=d.departmentId
+	left join CourseStudent cs on c.courseId=cs.coursescourseId
+	GROUP BY
+		c.courseId,
+		c.courseName,
+		c.budget,
+		d.departmentName,
+		d.departmentCode,
+		d.departmentHead) fq
+
+	--Second query
+	left join (
+		select 
+		a.courseId, 
+		cast((SUM(case when a.attendanceStatus=0 then 1 else 0 end)/cast(COUNT(a.attendanceId) as decimal))*100 as decimal(5,2)) as avgAttendance
+		from attendances a
+		group by courseId
+	) sq on fq.courseId=sq.courseid;
+
+
