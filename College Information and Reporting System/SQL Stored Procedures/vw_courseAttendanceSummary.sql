6fef1642aaae6ad52c127c9462c3d1b0972ed46c
Maryam <maryamgadiali@outlook.com>
2025-12-22 22:06:11 +0000

    Changed the get all attendance summaries sproc to a view

--- College Information and Reporting System/SQL Stored Procedures/vw_courseAttendanceSummary.sql
+++ College Information and Reporting System/SQL Stored Procedures/vw_courseAttendanceSummary.sql
@@ -1,21 +1,15 @@
-﻿USE [CIRS]
-GO
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
+﻿
 -- =============================================
 -- Author:		Maryam.G
 -- Create date: 20/12/2025
 -- Description:	Gets all attendance rates for courses
 -- =============================================
-CREATE PROCEDURE [dbo].[usp_GetAllAttendanceRateForCourses]
+CREATE VIEW vw_courseAttendanceSummary
 AS
-BEGIN
 	SELECT 
 	c.courseId,c.courseName,
 	count(*) as totalSessions,
 	SUM(CASE WHEN a.attendanceStatus=0 THEN 1 ELSE 0 END) as presentNumber
 	from attendances a inner join courses c on a.courseId=c.courseId
 	group by c.courseId, c.courseName
-END
+
